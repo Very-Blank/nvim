7eed80a11651cde13d8362411feb9836101a17be
Very-Blank <aapeli.saarelainen.76@gmail.com>
2024-11-08 12:23:29 +0000

    made easier to close windows with keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -3,6 +3,9 @@ return {
   vim.keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<cr>', { desc = 'Prev Tab', silent = true }),
   vim.keymap.set('n', '<leader>gf', '<cmd>Neotree toggle<cr>', { desc = 'Open NeoTree ', silent = true }),
 
+  vim.keymap.set('n', '<leader>wcl', '<cmd>BufferLineCloseRight<cr>', { desc = 'Close Right Window ', silent = true }),
+  vim.keymap.set('n', '<leader>wch', '<cmd>BufferLineCloseLeft<cr>', { desc = 'Close Left Window ', silent = true }),
+
   vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)'),
   vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)'),
   vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)'),
