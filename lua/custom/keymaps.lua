6488662ac7e1234f1a151236494871801afab560
Very-Blank <aapeli.saarelainen.76@gmail.com>
2024-11-08 11:26:26 +0000

    fixed bad keymap

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -6,4 +6,5 @@ return {
   vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)'),
   vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)'),
   vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)'),
+  vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }),
 }
