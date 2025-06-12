5f416cbab6d29b1362c3f5cdf48df5ca1055a0a3
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-12 10:13:41 +0300

    changes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -112,6 +112,16 @@ return {
             ['vim.lsp.util.stylize_markdown'] = true,
             ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
           },
+
+          signature = {
+            enabled = true,
+            auto_open = {
+              enabled = true,
+              trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+              luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+              throttle = 50, -- Debounce lsp signature help request by 50ms
+            }
+          },
         },
         presets = {
           inc_rename = false, -- enables an input dialog for inc-rename.nvim
