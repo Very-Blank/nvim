644d9889ee263e0103c227bbf6acfb4591245b2d
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-17 10:39:26 +0300

    fix to keymaps

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,5 +1,4 @@
 return {
-	vim.keymap.set('n', '<leader>gf', '<cmd>Neotree toggle<cr>', { desc = 'Open NeoTree ', silent = true }),
 	vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap-forward)'),
 	vim.keymap.set({ 'n', 'x', 'o' }, 'S', '<Plug>(leap-backward)'),
 	vim.keymap.set({ 'n', 'x', 'o' }, 'gs', '<Plug>(leap-from-window)'),
